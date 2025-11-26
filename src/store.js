cfb8c91248f0bc2a2e99caaeac754c0f96fbb172
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-11-26 17:46:00 +0100

    .........

--- src/store.js
+++ src/store.js
@@ -7,10 +7,8 @@ export const store = configureStore ({
     }
 })*/
 
-
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from './counterSlice'
-
+import counterReducer from "./counterSlice"
 
 export const store = configureStore({
     reducer: {
