b478296dded942ee5e4330f24a7b6b8408571ca8
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-12-02 17:41:27 +0100

    ...........

--- src/counterSlice.js
+++ src/counterSlice.js
@@ -4,7 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const counterSlice = createSlice({
   name: "counter",
    initialState: { value: 0 },
-  reducers: {
+    reducers: {
     increase: (state) => {
       state.value += 1;
     },
